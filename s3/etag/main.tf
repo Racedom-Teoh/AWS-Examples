f2c3f3457a6e1d2cc456e1311921e761f5efbd6c
Racedom <racedom0309@gmail.com>
2025-07-03 15:35:28 +0800

    Update main.tf

--- s3/etag/main.tf
+++ s3/etag/main.tf
@@ -7,11 +7,18 @@ terraform {
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 resource "aws_s3_bucket" "default" {
+  bucket = "my-terraform-test2-bucket2-unique-202507902"
 }
 
 resource "aws_s3_object" "object" {
-  bucket = resource.aws_s3_bucket.default
+  depends_on = [aws_s3_bucket.default]  # 確保在 S3 存儲桶創建後才創建物件
+  bucket = aws_s3_bucket.default.bucket
   key    = "myfile.txt"
   source = "myfile.txt"
+  etag   = filemd5("myfile.txt")
 }
